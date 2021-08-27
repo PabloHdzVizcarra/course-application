03e617749aca96f14d0676a7c2b825ac62f71a4f
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-27 09:45:15 -0500

    feat create basic UserRepository

---
+++ src/main/java/jvm/pablohdz/courseapplication/user/UserRepository.java
@@ -0,0 +1,20 @@
+package jvm.pablohdz.courseapplication.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Repository to handle persists users in the database
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * Obtains a {@link User} from the database by means of its username
+     *
+     * @param username The username to find
+     * @return A user from database
+     */
+    User getUserByUsername(String username);
+}
