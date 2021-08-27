ca345fd8367b4008b0682d6cb4a4144f05b26ff9
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-27 08:25:33 -0500

    first commit

---
+++ src/main/java/jvm/pablohdz/courseapplication/CourseApplication.java
@@ -0,0 +1,14 @@
+package jvm.pablohdz.courseapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+    // TODO: 8/27/21 config security
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApplication.class, args);
+    }
+
+}
