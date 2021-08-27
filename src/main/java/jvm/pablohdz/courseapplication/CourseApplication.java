06eb444ff27323cddca1c84d5dde204aa6ef19c8
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-27 11:43:04 -0500

    feat hash password user

--- src/main/java/jvm/pablohdz/courseapplication/CourseApplication.java
+++ src/main/java/jvm/pablohdz/courseapplication/CourseApplication.java
@@ -2,11 +2,19 @@ package jvm.pablohdz.courseapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CourseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CourseApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/jvm/pablohdz/courseapplication/user/UserServiceImpl.java
+++ src/main/java/jvm/pablohdz/courseapplication/user/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jvm.pablohdz.courseapplication.user;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -13,9 +14,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    // TODO: 8/27/21 hash password before saved user
     @Override
     public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         User userSaved = userRepository.save(user);
         log.info("New user created with the username name is: {}", user.getUsername());
         return userSaved;
