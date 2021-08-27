1d8aed0960e2b6d8d78a25faf2ca81a00fcd3464
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-27 08:25:46 -0500

    feat create basic security config

---
+++ src/main/java/jvm/pablohdz/courseapplication/controller/ExampleController.java
@@ -0,0 +1,15 @@
+package jvm.pablohdz.courseapplication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ExampleController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello Pablo";
+    }
+}
