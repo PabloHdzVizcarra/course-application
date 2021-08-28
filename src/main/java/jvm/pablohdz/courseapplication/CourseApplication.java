45e61a12fc910b1125ee35e04d6b970fec5b3ae2
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-28 09:27:24 -0500

    feat added bean to add users start app

--- src/main/java/jvm/pablohdz/courseapplication/CourseApplication.java
+++ src/main/java/jvm/pablohdz/courseapplication/CourseApplication.java
@@ -1,10 +1,15 @@
 package jvm.pablohdz.courseapplication;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import jvm.pablohdz.courseapplication.user.Gender;
+import jvm.pablohdz.courseapplication.user.User;
+import jvm.pablohdz.courseapplication.user.UserService;
 
 @SpringBootApplication
 public class CourseApplication {
@@ -16,5 +21,17 @@ public class CourseApplication {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveUser(new User(null, "Peter", "Parker",
+                    22, "Spiderman", "admin123", Gender.MALE,
+                    "test@test.com", null));
+            userService.saveUser(new User(null, "Tony", "Stark",
+                    42, "IronMan", "admin123", Gender.MALE,
+                    "example@test.com", null));
+        };
     }
 }
