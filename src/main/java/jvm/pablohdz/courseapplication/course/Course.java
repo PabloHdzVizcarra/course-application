4847ebd377f171ca5bc62c73964bfcff9674fef8
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-28 12:04:28 -0500

    feat added fields to table course

--- src/main/java/jvm/pablohdz/courseapplication/course/Course.java
+++ src/main/java/jvm/pablohdz/courseapplication/course/Course.java
@@ -1,11 +1,44 @@
 package jvm.pablohdz.courseapplication.course;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import jvm.pablohdz.courseapplication.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 
 @Entity
+@Table(name = "course")
+@ToString
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Course {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
     private Long id;
+
+    @NotNull
+    @Column(name = "course_name")
+    private String name;
+
+    @NotNull
+    @Column(name = "course_tag")
+    private String tag;
+
+    @ManyToMany
+    private Collection<User> users = new ArrayList<>();
 }
