72148985bb79c63e23d04fdfec0321dc2c823f22
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-30 12:37:04 -0500

    Feat created RoleService implementation

---
+++ src/main/java/jvm/pablohdz/courseapplication/user/RoleServiceImpl.java
@@ -0,0 +1,23 @@
+package jvm.pablohdz.courseapplication.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class RoleServiceImpl implements RoleService{
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        log.info("saved the role: {} in the database", role.getName());
+        return roleRepository.save(role);
+    }
+}
