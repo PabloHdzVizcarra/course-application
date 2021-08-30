e4688254d2af0f446d5f85989ce45a5e46b270f9
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-29 20:34:01 -0500

    Test to CourseServiceImpl

---
+++ src/test/java/jvm/pablohdz/courseapplication/course/CourseServiceImplTest.java
@@ -0,0 +1,31 @@
+package jvm.pablohdz.courseapplication.course;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+class CourseServiceImplTest {
+    @Mock
+    private CourseRepository courseRepository;
+
+    private CourseService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CourseServiceImpl(courseRepository);
+    }
+
+    @Test
+    void testThatFetchAllCourses() {
+        List<Course> users = underTest.getAllUsers();
+
+        assertNotNull(users);
+    }
+}
