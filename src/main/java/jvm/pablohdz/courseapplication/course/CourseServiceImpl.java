9badbca69db59faad4410aa71c13d85e790695eb
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-29 20:41:23 -0500

    Feat get all courses for persistence service

--- src/main/java/jvm/pablohdz/courseapplication/course/CourseServiceImpl.java
+++ src/main/java/jvm/pablohdz/courseapplication/course/CourseServiceImpl.java
@@ -2,17 +2,24 @@ package jvm.pablohdz.courseapplication.course;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class CourseServiceImpl implements CourseService{
+public class CourseServiceImpl implements CourseService {
     private final CourseRepository courseRepository;
 
     @Override
     public Course saveCourse(Course course) {
         return courseRepository.save(course);
+    }
+
+    @Override
+    public List<Course> getAllUsers() {
+        return courseRepository.findAll();
     }
 }
