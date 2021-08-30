f1b260422a9ce60c374fe569bed7c5f6cf369d58
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-29 20:52:13 -0500

    Feat fetch all users

--- src/main/java/jvm/pablohdz/courseapplication/course/CourseController.java
+++ src/main/java/jvm/pablohdz/courseapplication/course/CourseController.java
@@ -1,17 +1,20 @@
 package jvm.pablohdz.courseapplication.course;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/course")
 @RequiredArgsConstructor
-public class CourseResource {
+public class CourseController {
     private final CourseService courseService;
 
     @PostMapping
@@ -19,5 +22,10 @@ public class CourseResource {
         return ResponseEntity
                 .created(null)
                 .body(courseService.saveCourse(resource));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Course>> fetchUsers() {
+        return ResponseEntity.ok(courseService.getAllUsers());
     }
 }
