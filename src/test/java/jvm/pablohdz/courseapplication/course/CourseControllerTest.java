10995cf6e7376d01e50e218adfbb45b922b56fb7
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-29 21:21:40 -0500

    Test for save user method

--- src/test/java/jvm/pablohdz/courseapplication/course/CourseControllerTest.java
+++ src/test/java/jvm/pablohdz/courseapplication/course/CourseControllerTest.java
@@ -8,20 +8,36 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class CourseControllerTest {
     @Mock
     private CourseService courseService;
     private CourseController underTest;
+    private Course basicCourse;
 
     @BeforeEach
     void setUp() {
         underTest = new CourseController(courseService);
+        basicCourse = new Course(1L, "basic java",
+                "Java", new ArrayList<>());
+    }
+
+    @Test
+    void testThatSaveCourse() {
+        given(courseService.saveCourse(any()))
+                .willReturn(basicCourse);
+        ResponseEntity<Course> response = underTest.saveCourse(new Course());
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
     }
 
     @Test
