e478be770572652905cffd54fbf1bb2f7240ee34
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-29 19:36:59 -0500

    test controller response created to add course to user

--- src/test/java/jvm/pablohdz/courseapplication/user/UserControllerTest.java
+++ src/test/java/jvm/pablohdz/courseapplication/user/UserControllerTest.java
@@ -1,6 +1,7 @@
 package jvm.pablohdz.courseapplication.user;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -8,6 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import jvm.pablohdz.courseapplication.pojo.CourseToUserForm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,6 +32,14 @@ class UserControllerTest {
     @Test
     void createUserWhenDataRequestIsCorrect() {
         ResponseEntity<User> response = userController.saveUser(new User());
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Test to endpoint '/api/user/save-course' add course to user")
+    void addCourseToUser() {
+        ResponseEntity<?> response = userController.addCourseToUser(new CourseToUserForm());
 
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
     }
