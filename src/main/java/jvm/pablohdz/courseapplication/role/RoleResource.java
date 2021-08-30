7cdd76ed667845ce59f3c698266775a91e47345d
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-30 10:03:49 -0500

    Feat controller to handle role

---
+++ src/main/java/jvm/pablohdz/courseapplication/role/RoleResource.java
@@ -0,0 +1,12 @@
+package jvm.pablohdz.courseapplication.role;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/role")
+public class RoleResource {
+
+    // TODO: 8/30/21 endpoint to save role
+    // TODO: 8/30/21 endpoint to fetch all roles
+}
