fa99e1e864243d391226ef2a345e1f328507b6a6
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-31 20:17:51 -0500

    Feat added other exception to controller advice

--- src/main/java/jvm/pablohdz/courseapplication/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/jvm/pablohdz/courseapplication/exception/RestResponseEntityExceptionHandler.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import jvm.pablohdz.courseapplication.user.EmailUserDuplicatedException;
+import jvm.pablohdz.courseapplication.user.UserNotFoundException;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EmailUserDuplicatedException.class)
     protected ResponseEntity<Object> handleEmailDuplicated(EmailUserDuplicatedException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    protected ResponseEntity<Object> handleUserNotFound(UserNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
