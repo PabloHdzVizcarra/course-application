2b7d126627ea16daabcabd6474a7a4b883a64344
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-31 22:47:25 -0500

    Feat new method to fetch a role by name

--- src/main/java/jvm/pablohdz/courseapplication/role/RoleRepository.java
+++ src/main/java/jvm/pablohdz/courseapplication/role/RoleRepository.java
@@ -5,5 +5,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-
+    /**
+     * Fetch a role from the persistence service by means of its role name
+     *
+     * @param roleName the role name to find
+     * @return a role from the persistence service or null
+     */
+    Role findByName(String roleName);
 }
