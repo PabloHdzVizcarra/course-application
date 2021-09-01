718f7dfaf97d3a00f7ef7e5f8ac3644e21e2a410
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-31 22:53:12 -0500

    Feat implements method findByRoleName

--- src/main/java/jvm/pablohdz/courseapplication/role/RoleServiceImpl.java
+++ src/main/java/jvm/pablohdz/courseapplication/role/RoleServiceImpl.java
@@ -27,4 +27,9 @@ public class RoleServiceImpl implements RoleService {
     public List<Role> findRoles() {
         return roleRepository.findAll();
     }
+
+    @Override
+    public Role findByRoleName(String roleName) {
+        return roleRepository.findByName(roleName);
+    }
 }
