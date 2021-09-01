d1c9957372b005bc812b9bb22bfdcf0a18e45004
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-09-01 16:23:14 -0500

    fix add empty constructor

--- src/main/java/jvm/pablohdz/courseapplication/role/RoleDto.java
+++ src/main/java/jvm/pablohdz/courseapplication/role/RoleDto.java
@@ -1,10 +1,12 @@
 package jvm.pablohdz.courseapplication.role;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class RoleDto {
     private RoleName name;
 
